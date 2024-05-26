45feff3aea2d496c87425d92bee371a74a1cbee4
Michael Milewski <saramic@gmail.com>
2024-05-26 13:03:01 +1000

    🔧 add screenshots for error readability
    with:
      bundle add capybara-inline-screenshot

--- guestbook-rails-minimal/spec/support/capybara.rb
+++ guestbook-rails-minimal/spec/support/capybara.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
+require "capybara-inline-screenshot/rspec"
+
 Capybara.javascript_driver = :selenium_chrome
+
+Capybara::Screenshot.register_driver(:selenium_chrome) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
