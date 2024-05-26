8ccc088724281da14ed8b3779060c506b1566d68
Michael Milewski <saramic@gmail.com>
2024-05-26 17:24:52 +1000

    🧪 pending spec for a guestbook

---
+++ guestbook-rails-minimal/spec/features/guestbook_entry_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "Guestbook entry gets added and viewed", :js do
+  let(:guestbook) { Guestbook.new }
+
+  scenario "Guestbook entry gets added and viewed" do
+    When "the guestbook page is viewed" do
+      guestbook.load
+    end
+
+    Then "the guestbook is empty" do
+      pending "no entries and nothing loaded"
+      expect(guestbook.entries).to be_empty
+    end
+
+    When "a new entry is added" do
+      guestbook.new_entry.click
+      guestbook.fill_in(
+        title: "Amazing party",
+        text: "Party on, this year and next 🎉",
+        date: Time.zone.now
+      )
+      guestbook.submit!
+    end
+
+    Then "the visitor is told the message is successfully created" do
+      expect(guestbook.message).to eq "Entry was successfully created."
+    end
+
+    When "the user navigates home" do
+      guestbook.go_home.click
+    end
+
+    Then "the guestbook has the new message" do
+      expect(guestbook.entries.map(&:text)).to match([
+        /Party on, this year and next 🎉/
+      ])
+    end
+  end
+end
---
+++ guestbook-rails-minimal/spec/support/pages/guestbook.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Guestbook < SitePrism::Page
+  set_url Rails.application.routes.url_helpers.root_path
+end
