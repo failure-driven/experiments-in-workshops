2b92f3316f53eb83a03ccc005a49d4197aa87b5d
Michael Milewski <saramic@gmail.com>
2024-05-26 12:48:34 +1000

    🔧 extract PageObject model using site_prism
    with:
      bundle add site_prism
      bin/rspec

--- guestbook-rails-minimal/spec/features/it_works_spec.rb
+++ guestbook-rails-minimal/spec/features/it_works_spec.rb
@@ -3,20 +3,22 @@
 require "rails_helper"
 
 feature "It works, root rails demo page" do
+  let(:it_works_root) { ItWorksRoot.new }
+
   scenario "I have rails" do
     When "the root test page is visited" do
-      visit test_root_rails_path
+      it_works_root.load
     end
 
     Then "rails version is 7.1" do
       expect(
-        find("ul li", text: "Rails version").text
+        it_works_root.rails_version.text
       ).to match(/7\.1\.\d+/)
     end
 
     And "ruby version is 3.2" do
       expect(
-        find("ul li", text: "Ruby version").text
+        it_works_root.ruby_version.text
       ).to match(/3\.2\.\d+/)
     end
   end
