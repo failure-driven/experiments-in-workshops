03a96daf1d7a8c96b58ea9ec0073be783e20f4f7
Selena Small <selenawiththetattoo@gmail.com>
2024-12-19 14:29:10 +1100

    Simple Query from NextJS assuming you have data
    the data was created with original version of src/script.mjs
    
    Co-authored-by: Michael Milewski <saramic@gmail.com>

--- guestbook-nextjs/app/page.tsx
+++ guestbook-nextjs/app/page.tsx
@@ -1,9 +1,20 @@
 import "../public/styles/main.css";
+import { PrismaClient } from '@prisma/client'
 
-export default function Page() {
+export default async function Page() {
+  const prisma = new PrismaClient()
+  const users = await prisma.user.findMany()
+
   return (
     <div className="container">
       <h1>guest book</h1>
+      <div className="container">
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.email}, {user.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
