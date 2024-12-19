3e67d2769972c14df0d9eae3fc27b2168a24c77b
Selena Small <selenawiththetattoo@gmail.com>
2024-12-19 16:10:01 +1100

    WIP attempt to run jobs with BullMQ
    as per
      https://medium.com/@asanka_l/integrating-bullmq-with-nextjs-typescript-f41cca347ef8
    with:
        brew services start redis
        npm add bullmq dotenv ioredis
        npx tsx --watch src/workers/sample.worker.ts
        npm run worker:sample # did not work if dotenv is looking for a ruby version
        brew services stop redis
    
    Co-authored-by: Michael Milewski <saramic@gmail.com>

--- guestbook-nextjs/pages/api/submit.ts
+++ guestbook-nextjs/pages/api/submit.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import { Queue } from "bullmq";
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,5 +12,12 @@ export default async function handler(
       ...req.body,
     },
   });
+
+  const queue = new Queue("Comment");
+  const queueResponse = await queue.add("cars", {
+    id: result.id,
+    comment: result.comment,
+  });
+  console.log(queueResponse);
   return res.status(201).json(result);
 }
---
+++ guestbook-nextjs/src/workers/sample.worker.ts
@@ -0,0 +1,32 @@
+import { Worker, Queue } from "bullmq";
+import Redis from "ioredis";
+
+const connection = new Redis(process.env.REDIS_URL!);
+
+export const sampleQueue = new Queue("sampleQueue", {
+  connection,
+  defaultJobOptions: {
+    attempts: 2,
+    backoff: {
+      type: "exponential",
+      delay: 5000,
+    },
+  },
+});
+
+const worker = new Worker(
+  "sampleQueue", // this is the queue name, the first string parameter we provided for Queue()
+  async (job) => {
+    const data = job?.data;
+    console.log(data);
+    console.log("Task executed successfully");
+  },
+  {
+    connection,
+    concurrency: 5,
+    removeOnComplete: { count: 1000 },
+    removeOnFail: { count: 5000 },
+  },
+);
+
+export default worker;
