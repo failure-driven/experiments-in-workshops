16a19892abb0bd16cb5bac2f08c7e9dde1c9b662
Selena Small <selenawiththetattoo@gmail.com>
2024-12-19 15:36:02 +1100

    Added prettier
    npm run prettier:fix
    
    Co-authored-by: Michael Milewski <saramic@gmail.com>

--- guestbook-nextjs/cypress/e2e/site-navigation.cy.ts
+++ guestbook-nextjs/cypress/e2e/site-navigation.cy.ts
@@ -1,13 +1,13 @@
-describe('navigation of guest book site', () => {
+describe("navigation of guest book site", () => {
   Cypress.Keyboard.defaults({
     keystrokeDelay: 0,
-  })
+  });
 
-  it('displays the heading', () => {
-    cy.log("When I visit the site")
-    cy.visit('http://localhost:3000')
+  it("displays the heading", () => {
+    cy.log("When I visit the site");
+    cy.visit("http://localhost:3000");
 
-    cy.log("Then I should see the title")
-    cy.get('h1').should('have.text', 'guest book')
-  })
-})
+    cy.log("Then I should see the title");
+    cy.get("h1").should("have.text", "guest book");
+  });
+});
--- guestbook-nextjs/pages/api/submit.ts
+++ guestbook-nextjs/pages/api/submit.ts
@@ -1,15 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    const prisma = new PrismaClient()
-    const result = await prisma.user.create({
-        data: {
-            ...req.body,
-        },
-    })
-    return res.status(201).json(result)
+  const prisma = new PrismaClient();
+  const result = await prisma.user.create({
+    data: {
+      ...req.body,
+    },
+  });
+  return res.status(201).json(result);
 }
