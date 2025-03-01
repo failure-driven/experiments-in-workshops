88bc3f72c8b614f60589301cf4525acdf7addf44
Michael Milewski <saramic@gmail.com>
2025-03-01 01:47:02 +0000

    Allow codespaces to host development site

--- guestbook-rails-minimal/config/environments/development.rb
+++ guestbook-rails-minimal/config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Allow requests from GitHub Codespaces domain in development
+  config.hosts << /.*\.app\.github\.dev/
 end
