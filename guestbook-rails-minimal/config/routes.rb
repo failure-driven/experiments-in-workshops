61a92218798b7fcd7f1eadfebf07f599c8392c55
Michael Milewski <saramic@gmail.com>
2025-03-12 18:51:40 +1100

    Duplicate tests from 02 -> 03 for full implementation

--- guestbook-rails-minimal/config/routes.rb
+++ guestbook-rails-minimal/config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     resources :entries
     root "entries#index"
   end
+  namespace :guestbook_with_ai_full do
+    resources :entries
+    root "entries#index"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
---
+++ guestbook-rails-minimal/spec/features/03_user_adds_entry_to_guestbook_with_ai_full_spec.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "User adds entry to guestbook", :js do
+  let(:guestbook) { Pages::GuestbookWithAIFull.new }
+
+  context "when there are existing guestbook entries" do
+    before do
+      Entry.create!(name: "Gordon Great", text: "Presentation was great!")
+      Entry.create!(name: "Larry Learner", text: "Learnt a lot!")
+      Entry.create!(name: "Tyrel Tryer", text: "Can't wait to try these techniques at work!")
+    end
+
+    scenario "User adds entry to guestbook" do
+      When "the guestbook page is viewed" do
+        guestbook.load
+      end
+
+      Then "the guestbook has existing entries" do
+        expect(guestbook.entries_text).to eq([
+          "Presentation was great!",
+          "Learnt a lot!",
+          "Can't wait to try these techniques at work!"
+        ])
+      end
+
+      When "a new entry is added missing the name" do
+        guestbook.new_entry.click
+        guestbook.fill_in(
+          text: "Finally understood the benefits fo testing first"
+        )
+        guestbook.submit!
+      end
+
+      Then "the visitor is told there is an error as the name is blank" do
+        expect(guestbook.errors).to eq(["Name can't be blank"])
+      end
+
+      When "the visitor submits their form with their name" do
+        guestbook.fill_in(
+          name: "Positive Patricia"
+        )
+        guestbook.submit!
+      end
+
+      Then "the visitor is told the message is successfully created" do
+        expect(guestbook.message).to eq "Entry was successfully created."
+      end
+
+      Then "the guestbook has the new message" do
+        expect(guestbook).to have_entries(count: 4)
+        expect(guestbook.entries_text).to include(
+          "Finally understood the benefits fo testing first"
+        )
+      end
+    end
+
+    scenario "User adds entry to guestbook with AI" do
+      When "the guestbook page is viewed" do
+        guestbook.load
+      end
+
+      When "a new entry is added" do
+        guestbook.new_entry.click
+        guestbook.fill_in(
+          text: "like testing",
+          name: "Positive Patricia",
+          generate_ai_text: true
+        )
+        guestbook.submit!
+      end
+
+      Then "the visitor is told AI is generating the response" do
+        expect(guestbook.message).to eq "AI is generating the response, please wait..."
+      end
+
+      When "AI has finised generating the response" do
+        pending "need background jobs with sidekiq"
+        Sidekiq::Worker.drain_all
+      end
+
+      Then "the visitor is told the message is successfully created" do
+        expect(guestbook.message).to eq "Entry was successfully created."
+      end
+
+      Then "the guestbook has the new message" do
+        expect(guestbook).to have_entries(count: 4)
+        expect(guestbook.entries_text).to include(
+          "AI GENERATED like testing"
+        )
+      end
+    end
+  end
+end
