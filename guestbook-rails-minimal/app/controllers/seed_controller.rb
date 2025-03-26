f8836d4d2d0cbaa52a1e02861fb288df9ac661d8
Michael Milewski <saramic@gmail.com>
2025-03-26 21:17:06 +1100

    Settle on bulma for the time being

--- guestbook-rails-minimal/app/controllers/seed_controller.rb
+++ guestbook-rails-minimal/app/controllers/seed_controller.rb
@@ -1,6 +1,43 @@
 class SeedController < ApplicationController
   def index
     Entry.destroy_all
+    Entry.create!(
+      name: "Darlene",
+      text:
+        "Cool place, I guess. Might hack it later. Just kidding. Or am " +
+        "I? ;) Thanks for having me."
+    )
+    Entry.create!(
+      name: "Tyrell",
+      text:
+        "This is perfection. A place worthy of my presence. Thank you " +
+        "for hosting me in such an impeccable manner."
+    )
+    Entry.create!(
+      name: "Elliot",
+      text:
+        "Nice... I guess. But let’s be real, this world is a glitch. " +
+        "Nothing is as it seems. Thanks."
+    )
+    Message.destroy_all
+    Message.create!(
+      name: "Darlene",
+      text:
+        "Cool place, I guess. Might hack it later. Just kidding. Or am " +
+        "I? ;) Thanks for having me."
+    )
+    Message.create!(
+      name: "Tyrell",
+      text:
+        "This is perfection. A place worthy of my presence. Thank you " +
+        "for hosting me in such an impeccable manner."
+    )
+    Message.create!(
+      name: "Elliot",
+      text:
+        "Nice... I guess. But let’s be real, this world is a glitch. " +
+        "Nothing is as it seems. Thanks."
+    )
     redirect_to root_path
   end
 end
